42546fbcf895a2a5798dc22ba07df9707d0fd991
fathimaRushdha <aslamfathirush4@gmail.com>
2025-12-20 14:04:24 +0530

    gwu-hict-2022-20 I made

--- src/components/Therapist/TherapistLogin.jsx
+++ src/components/Therapist/TherapistLogin.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import {
+import
+ {
   containerStyle,
   formContainerStyle,
   headingStyle,
